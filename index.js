d5afde551ca7f18be42a0716dca987b746010442
Bennoh-1 <franciskibira017@gmail.com>
2022-06-15 12:38:40 +0300

    commit tree 8046aa5814d5b8cdcba1349683c86d560664c27d

--- index.js
+++ index.js
@@ -1,40 +1,43 @@
 // Code your solution in this file!
-function distanceFromHqInBlocks (blockNumber) {
-    if (blockNumber > 42) {
-      return blockNumber - 42;
-    } else {
-      return 42 - blockNumber;
-    }
+function distanceFromHqInBlocks(blockNumber){
+  if (blockNumber > 42){
+      let blocks = blockNumber - 42;
+      return blocks;
+  } else {
+      let blocks = 42 - blockNumber
+      return blocks;
   }
-  
+}
 
-  function distanceFromHqInFeet (blockNumber) {
-    return distanceFromHqInBlocks(blockNumber) * 264;
+function distanceFromHqInFeet(blockNumber){
+  if (blockNumber > 42){
+      let feet = ((blockNumber - 42) * 264)
+      return feet;
+  } else {
+      let feet = ((42 - blockNumber) * 264);
+      return feet;
   }
+}
 
-
-  
-  function distanceTravelledInFeet (start, destination) {
-    if (start < destination) {
-      return (destination - start) * 264;
-    } else {
-      return (start - destination) * 264;
-    }
+function distanceTravelledInFeet(start, destination){
+  if (destination > start){
+      let distanceInFeet = ((destination - start) * 264)
+      return distanceInFeet;
+  } else {
+      let distanceInFeet = ((start - destination) * 264)
+      return distanceInFeet
   }
-  
-  function calculatesFarePrice (start, destination) {
-    const distance = distanceTravelledInFeet(start, destination);
-  
-    if (distance <= 400) {
-        return 0;
-      } else if (distance > 400 && distance <= 2000) {
-        return .02 * (distance - 400);
-      } else if (distance > 2000 && distance < 2500) {
-        return 25;
-      } else {
-        return 'cannot travel that far';
+}
+
+function calculatesFarePrice(start, destination){
+  const distance = distanceTravelledInFeet(start, destination);
+      if (distance <= 400) {
+          return 0;
+      } else if (distance > 400 && distance <= 2000){
+          return (distance - 400) * .02
+      } else if (distance > 2000 && distance < 2500){
+          return 25 
+      } else if (distance > 2500){
+          return 'cannot travel that far'
       }
-    }
-
-    calculatesFarePrice(56, 34)
-  
+  }
